e06c6a74ee3a29eb91313fac48bc4a885599f38a
Josh Haws <joshhaws0@gmail.com>
2019-01-12 20:34:48 -0600

    add dashboard

---
+++ src/pages/Dashboard/Dashboard.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Dashboard.scss';
+
+class Dashboard extends Component {
+    render() {
+        return(
+            <div className="body-content">Hello</div>
+        )
+    }
+}
+
+export default Dashboard;
